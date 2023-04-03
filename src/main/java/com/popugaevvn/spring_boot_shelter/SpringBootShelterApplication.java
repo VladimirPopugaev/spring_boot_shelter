8958dfe4f4fee863be879a544e7aef863bf942f4
VladimirPopugaev <ux-00v@mail.ru>
2023-04-03 14:21:08 +0300

    init commit

---
+++ src/main/java/com/popugaevvn/spring_boot_shelter/SpringBootShelterApplication.java
@@ -0,0 +1,13 @@
+package com.popugaevvn.spring_boot_shelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popugaevvn/spring_boot_shelter/SpringBootShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popugaevvn.spring_boot_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
